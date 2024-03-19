5472cfd0b529b3a22c7e86ee5474223df801cfdf
InfinityLoop <127774659+InfinityLoop1@users.noreply.github.com>
2024-03-19 09:23:16 -0500

    Update game.js

--- game.js
+++ game.js
@@ -3,7 +3,12 @@ let xp = 0;
 let health = 100;
 let items = [];
 let name = prompt("What is your name?");
+update();
 
 function adventure() {
   
+}
+
+function update() {
+  document.getElementById("health").setAttribute("value", health);
 }
